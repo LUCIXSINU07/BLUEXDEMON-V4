677aee54d7e5b012ec1af4cbb33553fe467940a6
LUCIXSINU07 <bdreem78@gmail.com>
2024-12-19 18:42:22 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,9 +2,9 @@ const version = require("@whiskeysockets/baileys/package.json").version
 
 
 
-global.pairingNumber = "2348154574139"
+global.pairingNumber = "919674673966"
  
-global.botNumber = ["2348154574139"]
+global.botNumber = ["919674673966"]
 
 global.sign = "✘"
 // ☞ ➻ ➽ ☛ ✔
